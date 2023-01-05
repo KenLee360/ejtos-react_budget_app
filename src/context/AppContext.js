a8dacfa13142e28dcff6fe00095f6eeddd614599
KenLee360 <kennylee360@gmail.com>
2023-01-04 20:14:32 -0500

    Pushing Changes

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -57,13 +57,12 @@ export const AppReducer = (state, action) => {
                 ...state,
                 budget
             };
-        case 'SET_BUDGET':
-            action.type = "DONE";
-            state.budget = action.payload;
-
-            return {
-                ...state,
-            };
+            case 'SET_BUDGET':
+                action.type = "DONE";
+                state.budget = action.payload;
+                return {
+                    ...state,
+                };
         case 'CHG_CURRENCY':
             action.type = "DONE";
             state.currency = action.payload;
@@ -79,7 +78,7 @@ export const AppReducer = (state, action) => {
 // 1. Sets the initial state when the app loads
 const initialState = {
     budget: 2000,
-    expenses: [
+   expenses: [
         { id: "Marketing", name: 'Marketing', cost: 50 },
         { id: "Finance", name: 'Finance', cost: 300 },
         { id: "Sales", name: 'Sales', cost: 70 },
