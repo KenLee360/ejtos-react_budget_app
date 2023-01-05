29e536ce074993bf35ab951e7fb232d2c7e6711b
KenLee360 <kennylee360@gmail.com>
2023-01-04 20:15:11 -0500

    Staged

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,11 +2,29 @@ import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const {budget ,dispatch } = useContext(AppContext);
 
+    const handleClick = (value) => {
+        dispatch({
+            type: 'SET_BUDGET',
+            payload: value
+        })
+        
+    }
+    
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>Budget: £</span>
+            <input
+                    required='required'
+                    type='number'
+                    min='1660'
+                    max='20000'
+                    step='10'
+                    id='budget'
+                    value={budget}
+                    onChange={(event) => handleClick(event.target.value)}>
+                   </input>
         </div>
     );
 };
